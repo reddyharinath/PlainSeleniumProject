b24425013c37e459ff293a44b860d702b05053ae
reddyharinath <haridama67@gmail.com>
2022-04-28 08:29:24 +0530

    apr28 branch created and commited there with proper inital setup

--- src/main/java/basepackage/BaseClass.java
+++ src/main/java/basepackage/BaseClass.java
@@ -2,14 +2,18 @@ package basepackage;
 
 import java.io.FileInputStream;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import util.UtilClass;
 
 public class BaseClass {
 
-	public WebDriver driver;
-	public Properties prop ;
-	String configFilePath = "C:\\Users\\haridama67\\automation_project\\PlainSeleniumProject\\src\\main\\java\\testdata\\config.properties";
+	public static WebDriver driver;
+	public static Properties prop ;
+	//String configFilePath = "C:\\Users\\haridama67\\automation_project\\PlainSeleniumProject\\src\\main\\java\\testdata\\config.properties";
 	
 	public BaseClass() {
 		prop = new Properties();
@@ -20,5 +24,22 @@ public class BaseClass {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	@SuppressWarnings("deprecation")
+	public static void initlization() {
+		String browserName = prop.getProperty("browser");
+		if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/main/java/drivers/chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		driver.get(prop.getProperty("url"));
+		System.out.println("the page title after url load--->"+driver.getTitle());
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		//driver.manage().timeouts().pageLoadTimeout(UtilClass.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(UtilClass.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		
 	}
 }
--- src/test/java/testscripts/NavigateGoogleTest.java
+++ src/test/java/testscripts/NavigateGoogleTest.java
@@ -1,10 +1,5 @@
 package testscripts;
 
-import java.util.Properties;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,33 +13,19 @@ public class NavigateGoogleTest extends BaseClass {
 		super();
 	}
 	
-	public BaseClass baseClass;
-	public Properties prop;
-	public WebDriver driver;
-	public Registration registration;
+	Registration registration;
 	
-	public String chromeDriverPath = System.getProperty("user.dir")+"/src/main/java/drivers/chromedriver.exe";
-	//C:\Users\haridama67\automation_project\PlainSeleniumProject\src\main\java\drivers\chromedriver.exe
+	
 	@BeforeMethod
 	public void setUp() {
-		baseClass = new BaseClass();
-		prop = new Properties();
-		 
-		System.out.println("the chrome driver path--->"+chromeDriverPath);
-		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		initlization();
 	}
 	
 	@Test
 	public void navigateGoogleTest() {
 		registration = new Registration();
-		driver.get("https://demo.guru99.com/test/newtours/");
-		String title = driver.getTitle();
-		System.out.println("the title is-->" + title);
-		Assert.assertEquals(title, "Welcome: Mercury Tours");
-		//registration.clickOnRegisterLink();
-		//System.out.println("current url is -->"+driver.getCurrentUrl());
+		registration.clickOnSignUPLink();
+		System.out.println("the page title --->"+driver.getTitle());
 	}
 	
 	
